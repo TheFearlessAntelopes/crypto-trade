6de3c4b2acf72e035732d7f6a86f6d3e5ecfec98
lllevski <mitkostoikov1988@gmail.com>
2017-09-08 01:01:17 +0300

    changed mongo connection string to local database

--- server/config/config.js
+++ server/config/config.js
@@ -9,8 +9,8 @@ const envDbSecret = process.env.SECRET;
 const config = {
 	dev: {
 		port: envPort || 3000,
-		// connectionString: envConnString || 'mongodb://localhost:27017/crypto-store-db',
-		connectionString: envConnString || 'mongodb://thefearlessantilopes:ednodvetri@ds036617.mlab.com:36617/crypto-trade',
+		connectionString: envConnString || 'mongodb://localhost:27017/crypto-store-db',
+		// connectionString: envConnString || 'mongodb://thefearlessantilopes:ednodvetri@ds036617.mlab.com:36617/crypto-trade',
 		secretString: envDbSecret || 'Secret magical popcorn',
 	},
 	production: {
