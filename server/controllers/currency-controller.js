f6f5a0eec5c1c10f8c316c667b93e7af354cc41c
grozdanovgg <grozdanovgg@gmail.com>
2017-10-27 11:44:31 +0300

    node => nodemon

--- server/controllers/currency-controller.js
+++ server/controllers/currency-controller.js
@@ -4,6 +4,8 @@ const currencyConf = require('../config/currency-conf');
 module.exports = () => {
     return {
         getCoinList(req, res) {
+            console.log(currencyConf.coinListUrl);
+            console.log(req)
             fetch(currencyConf.coinListUrl)
                 .then((response) => {
                     return response.json();
@@ -31,34 +33,34 @@ module.exports = () => {
                         .json({ errorMessage: 'Could not fetch coin details!' });
                 })
         },
-        getPriceConversions(req,res){
+        getPriceConversions(req, res) {
             fetch(currencyConf.coinPrice + req.body.symbol + currencyConf.priceConversionValues)
-            .then((response) => {
-                return response.json();
-            })
-            .then((json) => {
-                return res.status(200)
-                    .json({ result: json })
-            })
-            .catch((err) => {
-                return res.status(400)
-                    .json({ errorMessage: 'Could not fetch coin prices!' });
-            })
+                .then((response) => {
+                    return response.json();
+                })
+                .then((json) => {
+                    return res.status(200)
+                        .json({ result: json })
+                })
+                .catch((err) => {
+                    return res.status(400)
+                        .json({ errorMessage: 'Could not fetch coin prices!' });
+                })
         },
-        getHistoryPrice(req, res){
+        getHistoryPrice(req, res) {
             fetch(currencyConf.currencyPricesHistoday + req.body.symbol +
-                 currencyConf.priceConversionForHistory + currencyConf.currencyOHLCLimit)
-            .then((response) => {
-                return response.json();
-            })
-            .then((json) => {
-                return res.status(200)
-                    .json({ result: json })
-            })
-            .catch((err) => {
-                return res.status(400)
-                    .json({ errorMessage: 'Could not fetch coin prices!' });
-            })
+                currencyConf.priceConversionForHistory + currencyConf.currencyOHLCLimit)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((json) => {
+                    return res.status(200)
+                        .json({ result: json })
+                })
+                .catch((err) => {
+                    return res.status(400)
+                        .json({ errorMessage: 'Could not fetch coin prices!' });
+                })
         }
     };
 };
